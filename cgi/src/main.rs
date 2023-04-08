b377b70632b57fc1b3572d512f7f637e8a46e5ac
Stéphane Bressani <stephane@bressani.dev>
2023-04-08 16:22:55 +0200

    avancée le cgi fonctionne

--- cgi/src/main.rs
+++ cgi/src/main.rs
@@ -1,5 +1,37 @@
 extern crate cgi;
 
-cgi::cgi_main! { |_request: cgi::Request| -> cgi::Response {
-     cgi::text_response(200, "Hello World")
-} }
+fn main() { cgi::handle(|request: cgi::Request| -> cgi::Response {
+    let request_str = format!("{:?}", &request);
+    let mut r: String = "".to_string();
+    let mut sw_found = false;
+    for x in request_str.split(" ") {
+        if sw_found {
+            r = x.to_string();
+            break;
+        }
+        if "\"x-cgi-query-string\":" == x {
+            sw_found = true;
+        }
+    }
+    r.pop();      // remove last
+    r.pop();
+    if r.len() > 0 {
+        r.remove(0);  // remove first
+    }
+    let mut params: Vec<(String, String)> = Vec::new();
+    for x in r.split("&") {
+        let mut a: String = "".to_string();
+        let mut b: String = "".to_string();
+        for (i, y) in x.split("=").enumerate() {
+            if i == 0 {
+                a = y.to_string();
+            } else if i == 1 {
+                b = y.to_string();
+            }
+        }
+        params.push((a, b));
+    }
+
+    let re = format!("{:?}", params);
+    cgi::text_response(200, re)
+})}
